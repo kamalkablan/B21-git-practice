1272ae0506001c2eb45011eb666fc65877087aae
kamalkablan <kimokablan10@gmail.com>
2021-01-26 13:28:26 -0500

    initial comment

---
+++ src/test/java/HelloTest.java
@@ -0,0 +1,8 @@
+public class HelloTest {
+    public static void main(String[] args) {
+        int num1 =1;
+        int num2 = 22;
+        int sum =  num1+num2;
+        System.out.println(sum);
+    }
+}
